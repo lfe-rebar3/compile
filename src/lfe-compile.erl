1398d3410cc1a5d218729250fdfd004b8d652ef8
Duncan McGreggor <oubiwann@gmail.com>
2015-10-19 23:07:32 -0500

    Copied newest rebar3 code over for LFE.

--- src/lfe-compile.erl
+++ src/lfe-compile.erl
@@ -42,29 +42,38 @@ init(State) ->
 do(State) ->
     rebar_api:debug("Starting do/1 for {lfe, compile} ...", []),
     DepsPaths = rebar_state:code_paths(State, all_deps),
-    AllApps = rebar_state:project_apps(State) ++ rebar_state:all_deps(State),
-    case rebar_state:get(State, escript_main_app, undefined) of
-        undefined ->
-            Dir = rebar_state:dir(State),
-            case rebar_app_discover:find_app(Dir, all) of
-                {true, AppInfo} ->
-                    case rebar_app_utils:find(rebar_app_info:name(AppInfo), AllApps) of
-                        {ok, AppInfo1} ->
-                            %% Use the existing app info instead of newly created one
-                            lfe_compiler:build_all(State, DepsPaths, AppInfo1);
-                        _ ->
-                            lfe_compiler:build_all(State, DepsPaths, AppInfo)
-                    end,
-                    {ok, State};
-                _ ->
-                    {error, {?MODULE, no_main_app}}
-            end;
-        Name ->
-            {ok, AppInfo} = rebar_app_utils:find(Name, AllApps),
-            {ok, lfe_compiler:build_all(State, DepsPaths, AppInfo)}
-    end.
+    PluginDepsPaths = rebar_state:code_paths(State, all_plugin_deps),
+    rebar_utils:remove_from_code_path(PluginDepsPaths),
+    code:add_pathsa(DepsPaths),
 
+    ProjectApps = rebar_state:project_apps(State),
+    Providers = rebar_state:providers(State),
+    Deps = rebar_state:deps_to_build(State),
+    Cwd = rebar_state:dir(State),
 
+    build_apps(State, Providers, Deps),
+    {ok, ProjectApps1} = rebar_digraph:compile_order(ProjectApps),
+
+    %% Run top level hooks *before* project apps compiled but *after* deps are
+    rebar_hooks:run_all_hooks(Cwd, pre, ?PROVIDER, Providers, State),
+
+    ProjectApps2 = build_apps(State, Providers, ProjectApps1),
+    State2 = rebar_state:project_apps(State, ProjectApps2),
+
+    ProjAppsPaths = [filename:join(rebar_app_info:out_dir(X), "ebin") || X <- ProjectApps2],
+    State3 = rebar_state:code_paths(State2, all_deps, DepsPaths ++ ProjAppsPaths),
+
+    rebar_hooks:run_all_hooks(Cwd, post, ?PROVIDER, Providers, State2),
+    case rebar_state:has_all_artifacts(State3) of
+        {false, File} ->
+            throw(?PRV_ERROR({missing_artifact, File}));
+        true ->
+            true
+    end,
+    rebar_utils:cleanup_code_path(rebar_state:code_paths(State3, default)
+                                 ++ rebar_state:code_paths(State, all_plugin_deps)),
+
+    {ok, State3}.
 
 -spec format_error(any()) -> iolist().
 format_error({missing_artifact, File}) ->
@@ -84,3 +93,78 @@ info(Description) ->
         "LFE. For more information, see the rebar documentation for~n"
         "'erl_opts'.~n",
         [Description]).
+
+build_apps(State, Providers, Apps) ->
+    [build_app(State, Providers, AppInfo) || AppInfo <- Apps].
+
+build_app(State, Providers, AppInfo) ->
+    AppDir = rebar_app_info:dir(AppInfo),
+    OutDir = rebar_app_info:out_dir(AppInfo),
+    copy_app_dirs(AppInfo, AppDir, OutDir),
+    compile(State, Providers, AppInfo).
+
+compile(State, AppInfo) ->
+    compile(State, rebar_state:providers(State), AppInfo).
+
+compile(State, Providers, AppInfo) ->
+    ?INFO("Compiling ~s", [rebar_app_info:name(AppInfo)]),
+    AppDir = rebar_app_info:dir(AppInfo),
+    AppInfo1 = rebar_hooks:run_all_hooks(AppDir, pre, ?PROVIDER,  Providers, AppInfo, State),
+
+    lfe_compiler:compile(AppInfo1),
+    case rebar_otp_app:compile(State, AppInfo1) of
+        {ok, AppInfo2} ->
+            AppInfo3 = rebar_hooks:run_all_hooks(AppDir, post, ?PROVIDER, Providers, AppInfo2, State),
+            has_all_artifacts(AppInfo3),
+            AppInfo3;
+        Error ->
+            throw(Error)
+    end.
+
+has_all_artifacts(AppInfo1) ->
+    case rebar_app_info:has_all_artifacts(AppInfo1) of
+        {false, File} ->
+            throw(?PRV_ERROR({missing_artifact, File}));
+        true ->
+            true
+    end.
+
+copy_app_dirs(AppInfo, OldAppDir, AppDir) ->
+    case ec_cnv:to_binary(filename:absname(OldAppDir)) =/=
+        ec_cnv:to_binary(filename:absname(AppDir)) of
+        true ->
+            EbinDir = filename:join([OldAppDir, "ebin"]),
+            %% copy all files from ebin if it exists
+            case filelib:is_dir(EbinDir) of
+                true ->
+                    OutEbin = filename:join(AppDir, "ebin"),
+                    filelib:ensure_dir(filename:join(OutEbin, "dummy.beam")),
+                    rebar_file_utils:cp_r(filelib:wildcard(filename:join(EbinDir, "*")), OutEbin);
+                false ->
+                    ok
+            end,
+
+            filelib:ensure_dir(filename:join(AppDir, "dummy")),
+
+            %% link or copy mibs if it exists
+            case filelib:is_dir(filename:join(OldAppDir, "mibs")) of
+                true ->
+                    %% If mibs exist it means we must ensure priv exists.
+                    %% mibs files are compiled to priv/mibs/
+                    filelib:ensure_dir(filename:join([OldAppDir, "priv", "dummy"])),
+                    symlink_or_copy(OldAppDir, AppDir, "mibs");
+                false ->
+                    ok
+            end,
+
+            %% link to src_dirs to be adjacent to ebin is needed for R15 use of cover/xref
+            SrcDirs = rebar_dir:all_src_dirs(rebar_app_info:opts(AppInfo), ["src"], []),
+            [symlink_or_copy(OldAppDir, AppDir, Dir) || Dir <- ["priv", "include"] ++ SrcDirs];
+        false ->
+            ok
+    end.
+
+symlink_or_copy(OldAppDir, AppDir, Dir) ->
+    Source = filename:join(OldAppDir, Dir),
+    Target = filename:join(AppDir, Dir),
+    rebar_file_utils:symlink_or_copy(Source, Target).
--- src/lfe_compiler.erl
+++ src/lfe_compiler.erl
@@ -11,98 +11,28 @@
          build_projs/2,
          build_apps/2,
          build_app/2,
-         compile/2,
-         lfe_compile/3]).
+         compile/2, compile/3]).
 
 %% ===================================================================
 %% Public API
 %% ===================================================================
 
-build_all(State, DepsPaths, AppInfo) ->
-    State1 = build_deps(State, DepsPaths),
-    State2 = build_projs(State1, DepsPaths),
-    compile(State2, AppInfo),
-    State2.
+compile(AppInfo) ->
+    Dir = ec_cnv:to_list(rebar_app_info:out_dir(AppInfo)),
+    compile(rebar_app_info:opts(AppInfo), Dir, filename:join([Dir, "ebin"])).
 
-build_deps(State, DepsPaths) ->
-    rebar_api:debug("Building dependencies:", []),
-    code:add_pathsa(DepsPaths),
-    PluginDepsPaths = rebar_state:code_paths(State, all_plugin_deps),
-    code:add_pathsa(PluginDepsPaths),
-    rebar_api:debug("Added code paths: ~p", [DepsPaths]),
-    Deps = rebar_state:deps_to_build(State),
-    EmptyState = rebar_state:new(),
-    DepsState = rebar_state:all_deps(EmptyState, rebar_state:all_deps(State)),
-    build_apps(DepsState, Deps),
-    State.
+compile(Opts, Dir, OutDir) ->
+    compile(Opts, Dir, OutDir, []).
 
-build_projs(State, DepsPaths) ->
-    rebar_api:debug("Building projects:", []),
-    DepsPaths = rebar_state:code_paths(State, all_deps),
-    ProjectApps = rebar_state:project_apps(State),
-    ProjectApps1 = build_apps(State, ProjectApps),
-    State1 = rebar_state:project_apps(State, ProjectApps1),
-    ProjAppsPaths = [filename:join(rebar_app_info:out_dir(X), "ebin") || X <- ProjectApps1],
-    State2 = rebar_state:code_paths(State1, all_deps, DepsPaths ++ ProjAppsPaths),
-    State2.
-
-build_apps(State, Apps) ->
-    rebar_api:debug("Building apps ...", []),
-    [build_app(State, AppInfo) || AppInfo <- Apps].
-
-build_app(State, AppInfo) ->
-    rebar_api:debug("\tBuilding app ~p ...", [rebar_app_info:name(AppInfo)]),
-    AppDir = rebar_app_info:dir(AppInfo),
-    OutDir = rebar_app_info:out_dir(AppInfo),
-    copy_app_dirs(State, AppDir, OutDir),
-
-    S = rebar_app_info:state_or_new(State, AppInfo),
-    S1 = rebar_state:all_deps(S, rebar_state:all_deps(State)),
-    compile(S1, AppInfo).
-
-compile(State, AppInfo) ->
-    AppDir = rebar_app_info:dir(AppInfo),
-    OutDir = filename:join(AppDir, "ebin"),
-    rebar_api:debug("\t\tCalculated outdir: ~p", [OutDir]), %% XXX DEBUG
-    lfe_compile(State, AppDir, OutDir),
-    AppInfo.
-
--spec lfe_compile(rebar_state:t(), file:name(), file:name()) -> 'ok'.
-lfe_compile(State, Dir, OutDir) ->
-    dotlfe_compile(State, Dir, OutDir).
+compile(Opts, Dir, OutDir, More) ->
+    ErlOpts = rebar_opts:erl_opts(Opts),
+    LfeOpts = check_files(rebar_opts:get(Opts, lfe_first_files, [])),
+    dotlfe_compile(Opts, Dir, OutDir, More, ErlOpts, LfeOpts).
 
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
 
-copy_app_dirs(State, OldAppDir, AppDir) ->
-    case ec_cnv:to_binary(filename:absname(OldAppDir)) =/=
-        ec_cnv:to_binary(filename:absname(AppDir)) of
-        true ->
-            EbinDir = filename:join([OldAppDir, "ebin"]),
-            %% copy all files from ebin if it exists
-            case filelib:is_dir(EbinDir) of
-                true ->
-                    OutEbin = filename:join(AppDir, "ebin"),
-                    filelib:ensure_dir(filename:join(OutEbin, "dummy.beam")),
-                    rebar_file_utils:cp_r(filelib:wildcard(filename:join(EbinDir, "*")), OutEbin);
-                false ->
-                    ok
-            end,
-            filelib:ensure_dir(filename:join(AppDir, "dummy")),
-            %% link to src_dirs to be adjacent to ebin is needed for R15 use of cover/xref
-            SrcDirs = rebar_dir:all_src_dirs(State, ["src"], ["test"]),
-            [symlink_or_copy(OldAppDir, AppDir, Dir) || Dir <- ["priv", "include"] ++ SrcDirs];
-        false ->
-            ok
-    end.
-
-symlink_or_copy(OldAppDir, AppDir, Dir) ->
-    Source = filename:join(OldAppDir, Dir),
-    Target = filename:join(AppDir, Dir),
-    rebar_file_utils:symlink_or_copy(Source, Target).
-
--spec dotlfe_compile(rebar_state:t(), file:filename(), file:filename()) -> ok.
 dotlfe_compile(State, Dir, OutDir) ->
     rebar_api:debug("\t\tStarting dotlfe_compile/3 ...", []), %% XXX DEBUG
     ErlOpts = rebar_utils:erl_opts(State),
@@ -134,7 +64,6 @@ dotlfe_compile(State, Dir, OutDir, MoreSources, ErlOpts, LfeFirstFiles) ->
 %%
 %% Ensure all files in a list are present and abort if one is missing
 %%
--spec check_files([file:filename()]) -> [file:filename()].
 check_files(FileList) ->
     [check_file(F) || F <- FileList].
 
@@ -144,17 +73,9 @@ check_file(File) ->
         true -> File
     end.
 
-gather_src([], Srcs) ->
-    Srcs;
-gather_src([Dir|Rest], Srcs) ->
-    gather_src(
-      Rest, Srcs ++ rebar_utils:find_files(Dir, ?RE_PREFIX".*\\.lfe\$")).
-
 target_base(OutDir, Source) ->
     filename:join(OutDir, filename:basename(Source, ".lfe")).
 
--spec internal_lfe_compile(rebar_config:config(), file:filename(), file:filename(),
-    file:filename(), list()) -> ok | {ok, any()} | {error, any(), any()}.
 internal_lfe_compile(Config, Dir, Module, OutDir, ErlOpts) ->
     Target = target_base(OutDir, Module) ++ ".beam",
     rebar_api:debug("\t\tCompiling~n\t\t\t~p~n\t\t\tto ~p ...", [Module, Target]),
