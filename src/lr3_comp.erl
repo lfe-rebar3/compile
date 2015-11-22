dc42fa3823125e904d32b455174e47e618f1feb4
Duncan McGreggor <oubiwann@gmail.com>
2015-11-22 11:28:21 -0600

    Moved block into its own function.

--- src/lr3_comp.erl
+++ src/lr3_comp.erl
@@ -33,7 +33,7 @@ compile(_State, Source, _AppDir, OutDir, ErlOpts) ->
     rebar_api:debug("\t\tOpts: ~p", [Opts]),
     CompileResults = lfe_comp:file(Source, Opts),
     rebar_api:debug("Compile results: ~p", [CompileResults]),
-    case lfe_comp:file(Source, Opts) of
+    case CompileResults of
         {ok, _Mod} ->
             ok;
         {ok, _Mod, Ws} ->
@@ -45,29 +45,28 @@ compile(_State, Source, _AppDir, OutDir, ErlOpts) ->
 compile_normal_apps(State) ->
     rebar_api:debug("Compiling normal LFE apps ...", []),
     Apps = lr3_comp_util:get_apps(State),
-    [begin
-         lr3_comp_util:copy_app_src(AppInfo),
-         Opts = rebar_app_info:opts(AppInfo),
-         AppDir = rebar_app_info:dir(AppInfo),
-         OtherSrcDirs = rebar_dir:src_dirs(Opts),
-         rebar_api:debug("OtherSrcDirs: ~p", [OtherSrcDirs]),
-         SourceDirs = lr3_comp_util:get_src_dirs(AppDir, ["src"] ++ OtherSrcDirs),
-         %%OutDir = 'lfe-compiler-util':out_dir(AppDir),
-         OutDir = filename:join(rebar_app_info:out_dir(AppInfo), "ebin"),
-         FirstFiles = lr3_comp_util:get_first_files(Opts, AppDir),
-         Files = lr3_comp_util:get_files(FirstFiles, SourceDirs),
-         rebar_api:debug("AppInfoDir: ~p", [AppDir]),
-         rebar_api:debug("SourceDirs: ~p", [SourceDirs]),
-         rebar_api:debug("OutDir: ~p", [OutDir]),
-         rebar_api:debug("FirstFiles: ~p", [FirstFiles]),
-         rebar_api:debug("Files: ~p", [Files]),
-         CompileFun = fun(Source, Opts1) ->
-                        rebar_api:console(" ~~~~> \tCompiling ~s ...",
-                                          [lr3_comp_util:relative(Source)]),
-                        compile(Opts1, Source, AppDir, OutDir)
-                      end,
-         rebar_base_compiler:run(Opts, [], Files, CompileFun)
-     end || AppInfo <- Apps],
+    [compile_normal_app(AppInfo) || AppInfo <- Apps],
     {ok, State}.
 
-
+compile_normal_app(AppInfo) ->
+    lr3_comp_util:copy_app_src(AppInfo),
+    Opts = rebar_app_info:opts(AppInfo),
+    AppDir = rebar_app_info:dir(AppInfo),
+    OtherSrcDirs = rebar_dir:src_dirs(Opts),
+    rebar_api:debug("OtherSrcDirs: ~p", [OtherSrcDirs]),
+    SourceDirs = lr3_comp_util:get_src_dirs(AppDir, ["src"] ++ OtherSrcDirs),
+    %%OutDir = 'lfe-compiler-util':out_dir(AppDir),
+    OutDir = filename:join(rebar_app_info:out_dir(AppInfo), "ebin"),
+    FirstFiles = lr3_comp_util:get_first_files(Opts, AppDir),
+    Files = lr3_comp_util:get_files(FirstFiles, SourceDirs),
+    rebar_api:debug("AppInfoDir: ~p", [AppDir]),
+    rebar_api:debug("SourceDirs: ~p", [SourceDirs]),
+    rebar_api:debug("OutDir: ~p", [OutDir]),
+    rebar_api:debug("FirstFiles: ~p", [FirstFiles]),
+    rebar_api:debug("Files: ~p", [Files]),
+    CompileFun = fun(Source, Opts1) ->
+                   rebar_api:console(" ~~~~> \tCompiling ~s ...",
+                                     [lr3_comp_util:relative(Source)]),
+                   compile(Opts1, Source, AppDir, OutDir)
+                 end,
+    rebar_base_compiler:run(Opts, [], Files, CompileFun).
