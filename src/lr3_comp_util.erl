627644ff99e8f5d7d79c0007038a57196d986e62
Eric Bailey <eric@ericb.me>
2016-07-29 20:47:05 -0500

    Remove unused functions from lr3_comp_util
    - copy_beam_files/2
    - get_files/2
    - target_file/2
    - target_base/2

--- src/lr3_comp_util.erl
+++ src/lr3_comp_util.erl
@@ -2,17 +2,13 @@
 
 -export([config/2,
          copy_app_src/1,
-         copy_beam_files/2,
          out_dir/0, out_dir/1,
          include_dir/0, include_dir/1,
          ensure_dir/1,
          get_apps/1,
          get_first_files/2,
-         get_files/2,
          get_src_dirs/2,
          relative_out_dir/1,
-         target_file/2,
-         target_base/2,
          relative/1]).
 
 -spec config(file:dirname(), list()) -> list().
@@ -30,21 +26,6 @@ copy_app_src(AppInfo) ->
     rebar_api:debug("\t\tAppFile: ~p", [AppFile]),
     rebar_api:debug("\t\tCopying ~p to ~p ...", [AppSrcFile, AppFile]),
     copy_file(AppSrcFile, AppFile).
-
-copy_beam_files(AppInfo, SrcEbinDir) ->
-    rebar_api:debug("\t\tEntered copy_beam_files/1 ...", []),
-    DstEbinDir = out_dir(rebar_app_info:dir(AppInfo)),
-    BeamFiles = filelib:wildcard(filename:join(SrcEbinDir, "*")),
-    rebar_api:debug("\t\tCopying ~p to ~p ...", [BeamFiles, DstEbinDir]),
-    [copy_beam_file(BeamFile, DstEbinDir) || BeamFile <- BeamFiles].
-
-copy_beam_file(BeamFile, EbinDir) ->
-    Filename = filename:basename(BeamFile),
-    DestFile = filename:join(EbinDir, Filename),
-    case BeamFile =:= DestFile of
-        true -> rebar_api:debug("\t\tFiles the same; skipping", []);
-        false -> copy_file(BeamFile, DestFile)
-    end.
 
 copy_file(Src, Dst) ->
     case file:copy(Src, Dst) of
@@ -92,23 +73,9 @@ get_first_files(Opts, AppDir) ->
     Dirs = rebar_opts:get(Opts, lfe_first_files, []),
     [filename:join(AppDir, Dir) || Dir <- Dirs].
 
-get_files(First, Dirs) ->
-    rebar_api:debug("\tDirs: ~p", [Dirs]),
-    Files = lists:append(
-              [rebar_utils:find_files(Dir, ".*\.lfe\$") || Dir <- Dirs]),
-    rebar_api:debug("\tFiles: ~p", [Files]),
-    NoDuplicates = lists:subtract(lists:usort(Files), First),
-    First ++ NoDuplicates.
-
 get_src_dirs(AppDir, Dirs) ->
     rebar_api:debug("\tDirs: ~p", [Dirs]),
     lists:usort([filename:join(AppDir, DirName) || DirName <- Dirs]).
-
-target_file(OutDir, Source) ->
-    target_base(OutDir, Source) ++ ".beam".
-
-target_base(OutDir, Source) ->
-    filename:join(OutDir, filename:basename(Source, ".lfe")).
 
 relative(Filename) ->
     {ok, Cwd} = file:get_cwd(),
